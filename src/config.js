28fa54664ddc72976c1fa2a0a9a1cad23fb26e4b
SnO2WMaN <sno2wman.official@gmail.com>
2019-10-05 17:21:15 +0900

    :art: Format code by prettier

---
+++ src/config.js
@@ -0,0 +1,24 @@
+const Conf = require('conf')
+
+const constants = require('./constants')
+const config = new Conf()
+
+const getAutoAdd = () => config.get(constants.AUTO_ADD)
+const getEmojiFormat = () => config.get(constants.EMOJI_FORMAT)
+const getSignedCommit = () => config.get(constants.SIGNED_COMMIT)
+const setAutoAdd = autoAdd => config.set(constants.AUTO_ADD, autoAdd)
+const setEmojiFormat = emojiFormat => {
+  config.set(constants.EMOJI_FORMAT, emojiFormat)
+}
+const setSignedCommit = signedCommit => {
+  config.set(constants.SIGNED_COMMIT, signedCommit)
+}
+
+module.exports = {
+  getAutoAdd,
+  getEmojiFormat,
+  getSignedCommit,
+  setAutoAdd,
+  setEmojiFormat,
+  setSignedCommit
+}
