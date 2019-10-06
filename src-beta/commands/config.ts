b76e999955cdab36307891d506c02c3e1e730e4d
SnO2WMaN <sno2wman.official@gmail.com>
2019-10-06 19:21:54 +0900

    ✨ Add commit title length config

--- src-beta/commands/config.ts
+++ src-beta/commands/config.ts
@@ -6,35 +6,38 @@ inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'))
 
 const config = new Conf()
 
-enum ConfigKeys {
+export enum ConfigKeys {
   AUTO_ADD = 'auto_add',
   EMOJI_FORMAT = 'emoji_format',
-  SIGNED_COMMIT = 'signed_commit'
+  SIGNED_COMMIT = 'signed_commit',
+  TITLE_MAX_LENGTH = 'title_max_length'
 }
 
 type Config = {
   [ConfigKeys.AUTO_ADD]: boolean
   [ConfigKeys.EMOJI_FORMAT]: 'emoji' | 'code'
   [ConfigKeys.SIGNED_COMMIT]: boolean
+  [ConfigKeys.TITLE_MAX_LENGTH]: number
 }
 
 export function getConfig(): Config {
   return {
     [ConfigKeys.AUTO_ADD]: config.get(ConfigKeys.AUTO_ADD, false),
-    [ConfigKeys.EMOJI_FORMAT]: config.get(ConfigKeys.AUTO_ADD, 'code'),
-    [ConfigKeys.SIGNED_COMMIT]: config.get(ConfigKeys.AUTO_ADD, false)
+    [ConfigKeys.EMOJI_FORMAT]: config.get(ConfigKeys.EMOJI_FORMAT, 'code'),
+    [ConfigKeys.SIGNED_COMMIT]: config.get(ConfigKeys.SIGNED_COMMIT, false),
+    [ConfigKeys.TITLE_MAX_LENGTH]: config.get(ConfigKeys.TITLE_MAX_LENGTH, 48)
   }
 }
 
 export default async function() {
   const answer = await inquirer.prompt([
     {
-      name: 'autoAdd',
+      name: ConfigKeys.AUTO_ADD,
       message: 'Enable automatic "git add ."',
       type: 'confirm'
     },
     {
-      name: 'emojiFormat',
+      name: ConfigKeys.EMOJI_FORMAT,
       message: 'Select how emojis should be used in commits',
       type: 'list',
       choices: [
@@ -43,12 +46,20 @@ export default async function() {
       ]
     },
     {
-      name: 'signedCommit',
+      name: ConfigKeys.SIGNED_COMMIT,
       message: 'Enable signed commits',
       type: 'confirm'
+    },
+    {
+      name: ConfigKeys.TITLE_MAX_LENGTH,
+      message: 'Max length of commit title',
+      type: 'number',
+      validate: number => number > 0 && number <= 72,
+      default: 48
     }
   ])
-  config.set(ConfigKeys.AUTO_ADD, answer.autoAdd)
-  config.set(ConfigKeys.EMOJI_FORMAT, answer.emojiFormat)
-  config.set(ConfigKeys.SIGNED_COMMIT, answer.signedCommit)
+  config.set(ConfigKeys.AUTO_ADD, answer[ConfigKeys.AUTO_ADD])
+  config.set(ConfigKeys.EMOJI_FORMAT, answer[ConfigKeys.EMOJI_FORMAT])
+  config.set(ConfigKeys.SIGNED_COMMIT, answer[ConfigKeys.SIGNED_COMMIT])
+  config.set(ConfigKeys.TITLE_MAX_LENGTH, answer[ConfigKeys.TITLE_MAX_LENGTH])
 }
