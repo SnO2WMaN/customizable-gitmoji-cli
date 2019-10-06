8031565a84d875ee76b8f2a950bbfb674f08604e
SnO2WMaN <sno2wman.official@gmail.com>
2019-10-07 01:11:32 +0900

    🐛 Exit if not git repository

--- src-beta/getHookPath.ts
+++ src-beta/getHookPath.ts
@@ -1,11 +1,16 @@
 import path from 'path'
 import SimpleGit from 'simple-git/promise'
+import consola from 'consola'
 
 const git = SimpleGit()
 
 export default async function() {
-  return path.join(
-    await git.revparse(['--absolute-git-dir']),
-    '/hooks/prepare-commit-msg'
-  )
+  try {
+    const gitPath = await git.revparse(['--absolute-git-dir'])
+    return path.join(await gitPath, '/hooks/prepare-commit-msg')
+  } catch (e) {
+    consola.error(e)
+    process.exit(1)
+    return ''
+  }
 }
