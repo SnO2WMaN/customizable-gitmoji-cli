9c9206af57a30c185e2de3c0a458036b5d09e006
SnO2WMaN <sno2wman.official@gmail.com>
2019-10-12 11:12:31 +0900

    🚑 Add shebang

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 import { terser } from 'rollup-plugin-terser'
+import shebang from 'rollup-plugin-add-shebang'
 
 export default {
   input: './src/cli.ts',
@@ -8,5 +9,10 @@ export default {
     file: './lib/cli.js',
     format: 'cjs'
   },
-  plugins: [typescript({ rollupCommonJSResolveHack: true }), json(), terser()]
+  plugins: [
+    typescript({ rollupCommonJSResolveHack: true }),
+    json(),
+    terser(),
+    shebang()
+  ]
 }
