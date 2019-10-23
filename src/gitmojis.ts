07beb1359ad90a2816a76978eedf475ce4a712ab
SnO2WMaN <sno2wman.official@gmail.com>
2019-10-23 21:48:41 +0900

    🐛 Fix config file loading

--- src/gitmojis.ts
+++ src/gitmojis.ts
@@ -1,20 +1,35 @@
+import consola from 'consola'
+import path from 'path'
+import chalk from 'chalk'
 import { Gitmoji, validate, Configuration } from './config'
 
-export function parsePackageName(preset: string): string {
-  if (preset.startsWith('@')) return preset
-  if (preset.startsWith('gitmoji-preset')) return preset
-  return `gitmoji-preset-${preset}`
+export function parsePackageName(preset: string): string[] {
+  if (preset.startsWith('@')) {
+    const splits = preset.split('/')
+    return splits.length === 3 // eslint-disable-line @typescript-eslint/no-magic-numbers
+      ? [`${splits[0]}/${splits[1]}`, splits[2]]
+      : [preset]
+  }
+  if (preset.startsWith('gitmoji-preset')) {
+    return preset.split('/')
+  }
+  return parsePackageName(`gitmoji-preset-${preset}`)
 }
 
 export default async function({ presets, rules, order }: Configuration) {
   const gitmojis: Gitmoji[] = []
   if (presets) {
+    const presetPaths = (Array.isArray(presets) ? presets : [presets]).map(
+      preset =>
+        path.resolve(process.cwd(), 'node_modules', ...parsePackageName(preset))
+    )
     await Promise.all(
-      (Array.isArray(presets) ? presets : [presets]).map(async preset => {
-        const { default: gitmojisOutput } = await import(
-          parsePackageName(preset)
-        )
-        if (validate('rules', gitmojisOutput)) gitmojis.push(...gitmojisOutput)
+      presetPaths.map(async presetPath => {
+        const { default: gitmojisOutput } = await import(presetPath)
+        if (validate('rules', gitmojisOutput)) {
+          gitmojis.push(...gitmojisOutput)
+          consola.info(`Completed loading presets ${chalk.blue(presetPath)}`)
+        }
       })
     )
   }
