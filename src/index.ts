327e35e7b69dc231463cf99a89b5f107d6f91143
SnO2WMaN <sno2wman.official@gmail.com>
2019-12-22 23:42:28 +0900

    ♻️ Refactor list command

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import cac from 'cac'
+import updateNotifier from 'update-notifier'
+import cosmiconfig from 'cosmiconfig'
+import parser from 'cz-gitmoji-config-parser'
+
+import package_ from '../package.json'
+
+import commands from './commands'
+
+updateNotifier({ pkg: package_ }).notify()
+
+const cli = cac('cz-gitmoji')
+
+async function getConfig() {
+  const result = await cosmiconfig('gitmoji2', { cache: false }).search()
+  return parser(result?.config ?? {})
+}
+
+cli.command('list', 'List all the available gitmojis').action(async () => {
+  await commands.list(await getConfig())
+})
+
+cli.version(package_.version)
+cli.help()
+cli.parse()
