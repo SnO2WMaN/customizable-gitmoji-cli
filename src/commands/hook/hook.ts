58377956b22ad47f3bf767f194aa9d4d8b7421b8
SnO2WMaN <sno2wman.official@gmail.com>
2019-12-24 01:26:36 +0900

    🐛 Fix commit hook

--- src/commands/hook/hook.ts
+++ src/commands/hook/hook.ts
@@ -2,5 +2,5 @@ export default {
   permissions: 0o775,
   path: '/hooks/prepare-commit-msg',
   contents:
-    '#!/bin/sh\n# gitmoji as a commit hook\nexec < /dev/tty\ngitmoji --hook $1\n'
+    '#!/bin/sh\n# gitmoji as a commit hook\nexec < /dev/tty\ncz-gitmoji commit --hook $1\n'
 }
